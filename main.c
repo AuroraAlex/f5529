808f06338a31f133322829979c8de38a9d21543d
yunxiso <634502342@qq.com>
2019-07-10 18:34:24 +0800

    main中添加了显示函数
    main中添加了几个显示函数

--- main.c
+++ main.c
@@ -5,12 +5,24 @@
 #define uint unsigned int
 #define ulong unsigned long
 
+unsigned char disp0[]={0x80+0,0x80+1,0x80+2,0x80+3,0x80+4,0};
+unsigned char disp1[]={0x80+5,0x80+6,0x80+7,0x80+8,0x80+9,':',0};
+unsigned char disp2[]={0x80+10,0x80+11,0x80+12,0x80+13,':',0};
+unsigned char disp3[]={0x80+14,0x80+15,0x80+16,':',0};
 
 int main( void )
 {
   // Stop watchdog timer to prevent time out reset
   WDTCTL = WDTPW + WDTHOLD;
+  system_init();
+  PrintString(3*8,0,disp0);
+  PrintString(0*8,2,disp1);
+  PrintString(0*8,4,disp2); 
+  PrintString(0*8,6,disp3);
+    
   
-  
-  return 0;
+  while(1)
+  {
+ 
+  }
 }
